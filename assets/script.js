5c07dc26af5396fa3a98f9af9975c3dbf1191712
Claire <claire@Claires-MacBook-Pro-3.local>
2021-11-11 15:58:53 -0500

    added function to color code textboxes based on time

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,26 @@
 var currentDay = document.querySelector("#currentDay");
 
 currentDay.textContent = moment().format("LL");
+
+var checkTime = function() {
+    for (var i = 9; i < 18; i++) {
+        var hourBlock = document.querySelector("#text-box" + i);
+        if (moment().isBefore(moment({hour:i}))) {
+            hourBlock.classList.add("past");
+        } else if (moment().isAfter(moment({hour:i})) && (moment().isBefore(moment({hour:i+1})))) {
+            hourBlock.classList.add("present");
+        } else {
+            hourBlock.classList.add("future");
+        }
+    }
+};
+
+var saveText = function() {
+    console.log("saved");
+};
+
+$("button").click(function() {
+    alert("clicked");
+});
+
+checkTime();
